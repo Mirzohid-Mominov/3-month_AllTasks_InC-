77fa8c459fe994d9da91ee970c5baf9756a0c8c2
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-10-05 12:38:31 +0500

    Updated

--- FirstCrud HT/Controllers/UserController.cs
+++ FirstCrud HT/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using FirstCrudAPI_Hometask.Services;
+﻿using FirstCrudAPI_Hometask.Models;
+using FirstCrudAPI_Hometask.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FirstCrud_HT.Controllers
@@ -14,11 +15,39 @@ namespace FirstCrud_HT.Controllers
             _userService = userService;
         }
 
-        [HttpGet]
-        public IActionResult GetAllUsers([FromQuery] int pageTooken, [FromQuery] int pageSize, [FromServices] UserService userService) 
+        [HttpGet("asd")]
+        public IActionResult GetAllUsers([FromQuery] int pageTooken, int pageSize, [FromServices] UserService userService) 
         {
-            var result = _userService.Get(user => true).Skip((pageTooken - 1) * pageSize).Take(pageSize).ToList();
+            var result = userService.Get(user => true).Skip((pageTooken - 1) * pageSize).Take(pageSize).ToList();
             return result.Any() ? Ok(result) : NotFound();
+        }
+
+        [HttpGet("{userId:guid}")]
+        public async ValueTask<IActionResult> GetById([FromRoute] Guid userId)
+        {
+            var result = await _userService.GetByIdAsync(userId);
+            return result is not null ? Ok(result) : NotFound(userId);
+        }
+
+        [HttpPost("user")]
+        public async ValueTask<IActionResult> CreateUser(User user)
+        {
+            var result = await _userService.CreateAsync(user);
+            return CreatedAtAction(nameof(GetById), new {userId = user.Id}, result);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateAsync([FromBody] User user)
+        {
+            await _userService.UpdateAsync(user);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteAsync([FromRoute] Guid userId)
+        {
+            await _userService.DeleteAsync(userId);
+            return NoContent();
         }
     }
 }
--- FirstCrud HT/Services/UserService.cs
+++ FirstCrud HT/Services/UserService.cs
@@ -17,5 +17,65 @@ namespace FirstCrudAPI_Hometask.Services
         {
             return _dataContext.Users.Where(predicate.Compile()).AsQueryable();
         }
+
+        public ValueTask<ICollection<User>> GetAsync(IEnumerable<Guid> ids, CancellationToken cancellation = default)
+        {
+            var users = _dataContext.Users.Where(user => ids.Contains(user.Id));
+            return new ValueTask<ICollection<User>>(users.ToList());
+        }
+
+        public ValueTask<User?> GetByIdAsync(Guid id, CancellationToken cancellation = default)
+        {
+            var user = _dataContext.Users.FirstOrDefault(user => user.Id == id);
+            return new ValueTask<User?>(user);
+        }
+
+        public async ValueTask<User> CreateAsync(User user, bool saveChanges = true, CancellationToken cancellation = default)
+        {
+            await _dataContext.Users.AddAsync(user, cancellation);
+            
+            if(saveChanges)
+            {
+                await _dataContext.SaveChangesAsync();
+            }
+
+            return user;
+        }
+
+        public async ValueTask<User> UpdateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
+        {
+            var foundUser = _dataContext.Users.FirstOrDefault(x => x.Id == user.Id);
+            if (user is null)
+                throw new InvalidOperationException("user not found with this information!");
+
+            foundUser.FirstName = user.FirstName;
+            foundUser.LastName = user.LastName;
+            foundUser.EmailAddress = user.EmailAddress;
+
+            await _dataContext.SaveChangesAsync();
+            return foundUser;
+        }
+
+        public async ValueTask<User> DeleteAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
+        {
+            var foundUser = await GetByIdAsync(user.Id, cancellationToken);
+            if(foundUser is null)
+                throw new InvalidOperationException("user not found with this information!");
+            
+            await _dataContext.Users.RemoveAsync(foundUser, cancellationToken);
+            await _dataContext.SaveChangesAsync();
+            return foundUser;
+        }
+
+        public async ValueTask<User> DeleteAsync(Guid guid, bool saveChanges = true, CancellationToken cancellationToken = default)
+        {
+            var foundUser = await GetByIdAsync(guid, cancellationToken);
+            if(foundUser is null)
+                throw new InvalidOperationException("user not found with this information!");
+
+            await _dataContext.Users.RemoveAsync(foundUser, cancellationToken);
+            await _dataContext.SaveChangesAsync();
+            return foundUser;
+        }
     }
 }
